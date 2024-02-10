57d0c9fe0c04fd24818c636188377c128da8ba25
unknown <denedoseikin@mail.ru>
2024-02-11 01:34:41 +0700

    feat: исправить валидацию

--- middlewares/validationJoi.js
+++ middlewares/validationJoi.js
@@ -42,6 +42,6 @@ module.exports.createMovieValidation = celebrate({
 
 module.exports.deleteMovieValidation = celebrate({
   params: Joi.object().keys({
-    movieId: Joi.string().required().length(24).hex(),
+    movieId: Joi.string().required(),
   }),
 });
